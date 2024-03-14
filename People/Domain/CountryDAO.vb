9d26f5c00c0377c520f3d936d2c4ae5e380fa7bd
JUAN MARÍA BRAVO LÓPEZ <JuanMaria.Bravo@alu.uclm.es>
2024-03-14 12:07:27 +0100

    Management of countries is completed

--- People/Domain/CountryDAO.vb
+++ People/Domain/CountryDAO.vb
@@ -13,8 +13,7 @@
         col = DBBroker.GetBroker().Read("SELECT * FROM Countries ORDER BY CountryID;")
         For Each aux In col ' for each element I get in the collection, I create a new Country object and add it to the collection
             c = New Country(aux(1).ToString)
-            ' c.CountryName = aux(2).ToString, same as the following line
-            c.ReadCountry() ' more simple if there are many properties
+            c.ReadCountry()
             Me.Countries.Add(c)
         Next
     End Sub
@@ -24,7 +23,7 @@
         col = DBBroker.GetBroker.Read("SELECT * FROM Countries WHERE CountryID='" & c.CountryID & "';")
         For Each aux In col
             c.CountryName = aux(2).ToString
-            c.CountryPopulation = aux(3)
+            c.CountryPopulation = Integer.Parse(aux(3).ToString)
         Next
     End Sub
 
