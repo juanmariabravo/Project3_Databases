abf91be7d61704c3776f12917c85faaa142d3d20
javialeem <100302751+javialeem@users.noreply.github.com>
2024-03-28 20:44:14 +0100

    Dominio actualizado

---
+++ People/Domain/GP.vb
@@ -0,0 +1,39 @@
+﻿Public Class GP
+    Public Property GPID As Integer
+    Public Property GPName As String
+    Public Property Countryid As Integer
+
+    Public ReadOnly Property GpDAO As GPDAO
+
+    Public Sub New()
+        Me.GpDAO = New GPDAO
+    End Sub
+
+    Public Sub New(ByVal id As Integer, ByVal name As String, ByVal country As Integer)
+        Me.GPID = id
+        Me.GPName = name
+        Me.Countryid = country
+
+        Me.GpDAO = New GPDAO
+    End Sub
+
+    Public Sub ReadAllGPs()
+        Me.GpDAO.ReadAll()
+    End Sub
+
+    Public Sub ReadGP()
+        Me.GpDAO.Read(Me)
+    End Sub
+
+    Public Function InsertGP() As Integer
+        Return Me.GpDAO.Insert(Me)
+    End Function
+
+    Public Function UpdateGP() As Integer
+        Return Me.GpDAO.Update(Me)
+    End Function
+
+    Public Function DeleteGP() As Integer
+        Return Me.GpDAO.Delete(Me)
+    End Function
+End Class
---
+++ People/Domain/GPs.vb
@@ -0,0 +1,3 @@
+﻿Public Class GPs
+
+End Class
