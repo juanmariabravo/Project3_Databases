40a4134e18ff986b2f21efc425ce15e357601dc7
javialeem <100302751+javialeem@users.noreply.github.com>
2024-04-27 01:41:27 +0200

    frmseasons updated

--- People/Presentation/frmSeasons.vb
+++ People/Presentation/frmSeasons.vb
@@ -1,4 +1,5 @@
-﻿Imports System.Linq.Expressions
+﻿Imports System.Drawing.Text
+Imports System.Linq.Expressions
 
 Public Class frmSeasons
     Private contract As Contract
@@ -27,97 +28,103 @@ Public Class frmSeasons
         races.Show()
 
     End Sub
+
+
     Private Sub frmSeasons_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Try
-            'Management of lists of Seasons, Teams and GPs
-            Dim season As New Season
-
-            season.ReadAllSeasons()
-
-            lstSeasons.Items.Clear()
-            For Each season In season.SeasonDAO.Seasons
-                lstSeasons.Items.Add(season.SeasonID)
-            Next
+            LoadSeasons()
+            DisableControls()
+            InitializeComboBoxes()
         Catch ex As Exception
-            MessageBox.Show("Error while reading seasons: " & ex.Message, ex.Source, MessageBoxButtons.OK, MessageBoxIcon.Error)
+            MessageBox.Show("Error while loading seasons: " & ex.Message, ex.Source, MessageBoxButtons.OK, MessageBoxIcon.Error)
         End Try
+    End Sub
+    Private Sub LoadSeasons()
+        Dim season As New Season
+        season.ReadAllSeasons()
+        lstSeasons.Items.Clear()
+        For Each seasonItem As Season In season.SeasonDAO.Seasons
+            lstSeasons.Items.Add(seasonItem.SeasonID)
+        Next
 
+    End Sub
 
-        'Management of visual elements
+    Private Sub DisableControls()
         btnDelete.Enabled = False
         btnDone.Enabled = False
         lstContracts.Enabled = False
         lstGPs.Enabled = False
         lblDriver1.Enabled = False
         lblDriver2.Enabled = False
-        ' Initialize combo box with integers from 5 to 10
-
-        CmbBoxMinTeams.Items.Add(" ")
-        CmbBoxMaxTeams.Items.Add(" ")
-        CmbBoxMinGPs.Items.Add(" ")
-        CmbBoxMaxGPs.Items.Add(" ")
-        For i As Integer = 5 To 10
-            CmbBoxMinTeams.Items.Add(i)
-            CmbBoxMaxTeams.Items.Add(i)
-        Next
-
-        ' Initialize combo box with integers from 10 to 20
-        For i As Integer = 10 To 20
-            CmbBoxMinGPs.Items.Add(i)
-            CmbBoxMaxGPs.Items.Add(i)
-        Next
-        ' Avoid user input in the combo boxes, only allow selection of the values and set the default selected value
-        CmbBoxMinTeams.DropDownStyle = ComboBoxStyle.DropDownList
-        CmbBoxMaxTeams.DropDownStyle = ComboBoxStyle.DropDownList
-        CmbBoxMinGPs.DropDownStyle = ComboBoxStyle.DropDownList
-        CmbBoxMaxGPs.DropDownStyle = ComboBoxStyle.DropDownList
-
-
     End Sub
+
+    Private Sub InitializeComboBoxes()
+        InitializeComboBox(CmbBoxMinTeams, 5, 10)
+        InitializeComboBox(CmbBoxMaxTeams, 5, 10)
+        InitializeComboBox(CmbBoxMinGPs, 10, 20)
+        InitializeComboBox(CmbBoxMaxGPs, 10, 20)
+    End Sub
+
+    Private Sub InitializeComboBox(comboBox As ComboBox, minValue As Integer, maxValue As Integer)
+        comboBox.Items.Add(" ")
+        For i As Integer = minValue To maxValue
+            comboBox.Items.Add(i)
+        Next
+        comboBox.DropDownStyle = ComboBoxStyle.DropDownList
+    End Sub
+
     Private Sub LstSeason_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstSeasons.SelectedIndexChanged
-
         If lstSeasons.SelectedIndex >= 0 AndAlso lstSeasons.SelectedItems.Count = 1 Then
-
-            Dim season As New Season()
-            Dim auxGPsID As Integer
-            Dim auxTeam As Team
-            Dim auxTeamID As Integer
-            Dim auxGP As GP
-            season.SeasonID = Integer.Parse(lstSeasons.SelectedItem.ToString)
             Try
-                season.ReadSeason()
+                Dim seasonID As Integer = CInt(lstSeasons.SelectedItem.ToString)
+                lblDriver1.Text = " "
+                lblDriver2.Text = " "
+                LoadSeasonData(seasonID)
 
-
-                lstContracts.Items.Clear()
-                lstGPs.Items.Clear()
-
-                ' Enable the list of GPs and contracts to insert the names of the contracts and the names of the GPs
-                lstContracts.Enabled = True
-                lstGPs.Enabled = True
-                btnDelete.Enabled = True
-
-                ' Insert the ids + names of the teams that have a contract in the list of contracts
-                For Each auxTeamID In season.ListContractsTeamID
-
-                    auxTeam = New Team(auxTeamID)
-                    auxTeam.ReadTeam()
-
-                    lstContracts.Items.Add(auxTeam.TeamID & " " & auxTeam.TeamName)
-                Next
-
-                ' Insert the names of the GPs in the list of GPs
-                For Each auxGPsID In season.ListGPsID
-                    auxGP = New GP(auxGPsID)
-                    auxGP.ReadGP()
-
-                    lstGPs.Items.Add(auxGP.GPID & " " & auxGP.GPName)
-                Next
             Catch ex As Exception
-                MessageBox.Show("Error while reading seasons: " & ex.Message, ex.Source, MessageBoxButtons.OK, MessageBoxIcon.Error)
+                MessageBox.Show("Error while processing selected season: " & ex.Message, ex.Source, MessageBoxButtons.OK, MessageBoxIcon.Error)
             End Try
         End If
+    End Sub
+
+    Private Sub LoadSeasonData(seasonID As Integer)
+        Dim season As New Season()
+        season.SeasonID = seasonID
+        season.ReadSeason()
+        ClearListBoxes()
+        EnableListBoxes()
+        InsertContracts(season)
+        InsertGPs(season)
 
     End Sub
+
+    Private Sub ClearListBoxes()
+        lstContracts.Items.Clear()
+        lstGPs.Items.Clear()
+    End Sub
+
+    Private Sub EnableListBoxes()
+        lstContracts.Enabled = True
+        lstGPs.Enabled = True
+        btnDelete.Enabled = True
+    End Sub
+
+    Private Sub InsertContracts(season As Season)
+        For Each teamID As Integer In season.ListContractsTeamID
+            Dim team As New Team(teamID)
+            team.ReadTeam()
+            lstContracts.Items.Add(team.TeamID & " " & team.TeamName)
+        Next
+    End Sub
+
+    Private Sub InsertGPs(season As Season)
+        For Each gpID As Integer In season.ListGPsID
+            Dim gp As New GP(gpID)
+            gp.ReadGP()
+            lstGPs.Items.Add(gp.GPID & " " & gp.GPName)
+        Next
+    End Sub
+
 
     Private Sub lstContracts_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstContracts.SelectedIndexChanged
         Try
@@ -129,8 +136,8 @@ Public Class frmSeasons
 
                 If lstContracts.SelectedIndex >= 0 Then
                     Dim tokens As String() = lstContracts.SelectedItem.ToString.Split(" "c)
-                    seasonID = Integer.Parse(lstSeasons.SelectedItem.ToString)
-                    contracID = Integer.Parse(tokens(0))
+                    seasonID = CInt(lstSeasons.SelectedItem.ToString)
+                    contracID = CInt(tokens(0))
                     contract = New Contract(contracID, seasonID)
                     contract.ReadContract()
 
@@ -148,80 +155,20 @@ Public Class frmSeasons
         End Try
 
     End Sub
+
     Private Sub btnGenerateSeasonInfo_Click(sender As Object, e As EventArgs) Handles btnGenerateSeasonInfo.Click
         Dim minTeams As Integer
         Dim maxTeams As Integer
         Dim minGPs As Integer
         Dim maxGPs As Integer
         Dim seasonYear As Integer
-        Dim i As Integer
+        getSeasonYear(seasonYear)
+        getnumTeams(minTeams, maxTeams)
+        getnumGPs(minGPs, maxGPs)
 
-        'Check for format in the year of the season --------------------------------
-        Try
-            'Check if the year is already created, then cancel creation
-            For i = 1 To lstSeasons.Items.Count
-                If lstSeasons.Items(i - 1).ToString = txtSeasonYear.Text Then
-                    MessageBox.Show("The year of the season is already in the list of seasons.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-                    txtSeasonYear.Clear()
-                    txtSeasonYear.Focus()
-                    Exit Sub
-                End If
-            Next
-            'Check for the selection of the year of the season
-            If txtSeasonYear.TextLength = 4 AndAlso Integer.Parse(txtSeasonYear.Text) >= 1970 AndAlso Integer.Parse(txtSeasonYear.Text) <= 2024 Then
-                seasonYear = Integer.Parse(txtSeasonYear.Text)
-            Else
-                MessageBox.Show("The year of the season must be between 1970 and 2024.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-                txtSeasonYear.Clear()
-                txtSeasonYear.Focus()
-                Exit Sub
-            End If
-
-            'Check for the selection of the number of teams and GPs
-            If CmbBoxMinTeams.SelectedItem IsNot " " AndAlso CmbBoxMaxTeams.SelectedItem IsNot " " AndAlso CmbBoxMinTeams.SelectedIndex <= CmbBoxMaxTeams.SelectedIndex Then
-                If CmbBoxMinTeams.SelectedIndex >= 0 AndAlso CmbBoxMaxTeams.SelectedIndex >= 0 Then
-                    minTeams = Integer.Parse(CmbBoxMinTeams.SelectedItem.ToString)
-                    maxTeams = Integer.Parse(CmbBoxMaxTeams.SelectedItem.ToString)
-                Else
-                    minTeams = 5
-                    maxTeams = 10
-                End If
-            Else
-                MessageBox.Show("The minimum number of teams must be less than the maximum number of teams.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-                Exit Sub
-            End If
-
-
-            'We do the same with the controls of Combobox for the number of GPs
-            If CmbBoxMinGPs.SelectedItem IsNot " " AndAlso CmbBoxMaxGPs.SelectedItem IsNot " " AndAlso CmbBoxMinGPs.SelectedIndex <= CmbBoxMaxGPs.SelectedIndex Then
-                If CmbBoxMinGPs.SelectedIndex >= 0 AndAlso CmbBoxMaxGPs.SelectedIndex >= 0 Then
-                    minGPs = Integer.Parse(CmbBoxMinGPs.SelectedItem.ToString)
-                    maxGPs = Integer.Parse(CmbBoxMaxGPs.SelectedItem.ToString)
-                Else
-                    minGPs = 10
-                    maxGPs = 20
-                End If
-            Else
-                MessageBox.Show("The minimum number of GPs must be less than the maximum number of GPs.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-                Exit Sub
-            End If
-
-
-        Catch ex As Exception
-            MessageBox.Show("Error while Fetching Data: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-        End Try
-        'Take all of the info for the season creation --------------------------------
-        Try
-
-            'Random number of Teams
-            numberTeams = New Random().Next(minTeams, maxTeams + 1)
-
-            'Random number of GPs
-            numberGPs = New Random().Next(minGPs, maxGPs + 1)
-
-        Catch ex As Exception
-            MessageBox.Show("Error while getting random numbers: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-        End Try
+        If Not ShouldCreateSeason(minTeams, maxTeams, minGPs, maxGPs) Then
+            Exit Sub
+        End If
 
         Dim season As New Season(seasonYear)
         season.ReadSeason()
@@ -237,15 +184,126 @@ Public Class frmSeasons
                 lstSeasons.Items.Add(season.SeasonID)
             Next
         Catch ex As Exception
-            MessageBox.Show("Error" & ex.Message, ex.Source, MessageBoxButtons.OK, MessageBoxIcon.Error)
+            MessageBox.Show("Error when inserting the season:" & ex.Message, ex.Source, MessageBoxButtons.OK, MessageBoxIcon.Error)
+            season.DeleteSeason()
         End Try
 
+
     End Sub
+
+    Private Sub getSeasonYear(ByRef seasonYear As Integer)
+
+        'Check if the year is already created, then cancel creation
+        For i = 1 To lstSeasons.Items.Count
+            If lstSeasons.Items(i - 1).ToString = txtSeasonYear.Text Then
+                MessageBox.Show("The year of the season is already in the list of seasons.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                txtSeasonYear.Clear()
+                txtSeasonYear.Focus()
+                Exit Sub
+            End If
+        Next
+        'Check for the selection of the year of the season
+        If txtSeasonYear.TextLength = 4 AndAlso Integer.Parse(txtSeasonYear.Text) >= 1970 AndAlso CInt(txtSeasonYear.Text) <= 2024 Then
+            seasonYear = CInt(txtSeasonYear.Text)
+        Else
+            MessageBox.Show("The year of the season must be between 1970 and 2024.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            txtSeasonYear.Clear()
+            txtSeasonYear.Focus()
+            Exit Sub
+        End If
+    End Sub
+
+    Private Sub getnumTeams(ByRef minTeams As Integer, ByRef maxTeams As Integer)
+        Try
+
+            ControlNumberTeamsValues(minTeams, maxTeams)
+            'Check for the selection of the number of teams and GPs
+            If CmbBoxMinTeams.SelectedItem IsNot " " AndAlso CmbBoxMaxTeams.SelectedItem IsNot " " AndAlso CmbBoxMinTeams.SelectedIndex <= CmbBoxMaxTeams.SelectedIndex Then
+                If CmbBoxMinTeams.SelectedIndex >= 0 AndAlso CmbBoxMaxTeams.SelectedIndex >= 0 Then
+                    minTeams = Integer.Parse(CmbBoxMinTeams.SelectedItem.ToString)
+                    maxTeams = Integer.Parse(CmbBoxMaxTeams.SelectedItem.ToString)
+                Else
+                    minTeams = 5
+                    maxTeams = 10
+                End If
+                numberTeams = New Random().Next(minTeams, maxTeams + 1)
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Error while getting random numbers: An error occurred while generating random numbers.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+        End Try
+    End Sub
+
+    Private Sub getnumGPs(ByRef minGps As Integer, ByRef maxGps As Integer)
+        Try
+
+            ControlNumberGpsValues(minGps, maxGps)
+            'Check for the selection of the number of teams and GPs
+            If CmbBoxMinTeams.SelectedItem IsNot " " AndAlso CmbBoxMaxGPs.SelectedItem IsNot " " AndAlso CmbBoxMinGPs.SelectedIndex <= CmbBoxMaxGPs.SelectedIndex Then
+                If CmbBoxMinGPs.SelectedIndex >= 0 AndAlso CmbBoxMaxGPs.SelectedIndex >= 0 Then
+                    minGps = Integer.Parse(CmbBoxMinGPs.SelectedItem.ToString)
+                    maxGps = Integer.Parse(CmbBoxMaxGPs.SelectedItem.ToString)
+                Else
+                    minGps = 10
+                    maxGps = 20
+                End If
+                numberGPs = New Random().Next(minGps, maxGps + 1)
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Error while getting random numbers: An error occurred while generating random numbers.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+    Private Sub ControlNumberTeamsValues(ByRef minTeams As Integer, ByRef maxTeams As Integer)
+        'Control cases in which only one combobox is selected
+        If CmbBoxMinTeams.SelectedIndex > 0 Then
+
+            minTeams = Integer.Parse(CmbBoxMinTeams.SelectedItem.ToString)
+        Else
+            minTeams = 5
+        End If
+        If CmbBoxMaxTeams.SelectedIndex > 0 Then
+            maxTeams = Integer.Parse(CmbBoxMaxTeams.SelectedItem.ToString)
+        Else
+            maxTeams = 10
+        End If
+        numberTeams = New Random().Next(minTeams, maxTeams + 1)
+    End Sub
+
+    Private Sub ControlNumberGpsValues(ByRef minGps As Integer, ByRef maxGps As Integer)
+        'Control cases in which only one combobox is selected
+        If CmbBoxMinGPs.SelectedIndex > 0 Then
+
+            minGps = Integer.Parse(CmbBoxMinGPs.SelectedItem.ToString)
+        Else
+            minGps = 10
+        End If
+        If CmbBoxMaxGPs.SelectedIndex > 0 Then
+            maxGps = Integer.Parse(CmbBoxMaxGPs.SelectedItem.ToString)
+        Else
+            maxGps = 20
+        End If
+        numberGPs = New Random().Next(minGps, maxGps + 1)
+
+    End Sub
+    Private Function ShouldCreateSeason(minTeams As Integer, maxTeams As Integer, minGps As Integer, maxGps As Integer) As Boolean
+        If minTeams > maxTeams Then
+            MessageBox.Show("The minimum number of teams must be less than or equal to the maximum number of teams.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return False
+        End If
+
+        If minGps > maxGps Then
+            MessageBox.Show("The minimum number of GPs must be less than or equal to the maximum number of GPs.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return False
+        End If
+
+        Return True
+    End Function
+
 
     Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
         ' Delete the season selected in the list of seasons
         Try
-            Dim season As New Season(Integer.Parse(lstSeasons.SelectedItem.ToString))
+            Dim season As New Season(CInt(lstSeasons.SelectedItem.ToString))
             season.ReadSeason()
             season.DeleteSeason()
             lstSeasons.Items.Clear()
@@ -253,6 +311,8 @@ Public Class frmSeasons
             For Each season In season.SeasonDAO.Seasons
                 lstSeasons.Items.Add(season.SeasonID)
             Next
+            lstContracts.Items.Clear()
+            lstGPs.Items.Clear()
             btnDelete.Enabled = False
         Catch ex As Exception
             MessageBox.Show("Error while deleting the season: " & ex.Message, ex.Source, MessageBoxButtons.OK, MessageBoxIcon.Error)
